acc043a61405c419d04ed47986babe5ed9867682
‪Ahmed Aly Mohamed <ahmedalimohamed1994@gmail.com>
2021-01-17 17:33:43 +0200

    enhancing responsive course card

--- public/js/ui.js
+++ public/js/ui.js
@@ -3,14 +3,26 @@ $(document).ready(() => {
     // Truncate text of course card title
 
     truncateCourseCardTitle();
+    truncateCourseCardText();
 
     function truncateCourseCardTitle() {
         var cardTitles = $(".card-title a");
         $.each(cardTitles, function (key, value) {
             var titleText = value.text;
-            if (titleText.length > 120) {
-                newTitle = titleText.substring(0, 120).concat("..");
+            if (titleText.length > 90) {
+                newTitle = titleText.substring(0, 90).concat("..");
                 $(this).text(newTitle);
+            }
+        });
+    }
+
+    function truncateCourseCardText() {
+        var cardTexts = $(".card-text");
+        $.each(cardTexts, function (key, value) {
+            var text = value.innerText;
+            if (text.length > 116) {
+                newText = text.substring(0, 116).concat("......");
+                $(this).text(newText);
             }
         });
     }
