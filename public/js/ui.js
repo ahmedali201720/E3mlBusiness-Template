888b1f081d038ca9f49681bec5cb16e8bd7015f0
‪Ahmed Aly Mohamed <ahmedalimohamed1994@gmail.com>
2021-01-18 17:43:14 +0200

    working on filter

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,3 +1,6 @@
+// ***************************************************************************************
+// Function to get language
+// ***************************************************************************************
 function getLang() {
     var lang = $('html').attr('lang');
     if (lang == 'ar') {
@@ -8,12 +11,20 @@ function getLang() {
     }
 }
 
+// ***************************************************************************************
+// Something to do when document is ready
+// ***************************************************************************************
+
 $(document).ready(() => {
 
     // Truncate text of course card title
     truncateCourseCardTitle();
     truncateCourseCardText();
 
+    // Function to handle filter state
+    handleFilterState();
+
+    // Function to truncate course card title
     function truncateCourseCardTitle() {
         var cardTitles = $(".card-title a");
         $.each(cardTitles, function (key, value) {
@@ -25,6 +36,7 @@ $(document).ready(() => {
         });
     }
 
+    // Function to truncate course card text
     function truncateCourseCardText() {
         var cardTexts = $(".card-text");
         $.each(cardTexts, function (key, value) {
@@ -36,4 +48,74 @@ $(document).ready(() => {
         });
     }
 
-})
+    // function to handle filter state on load
+    function handleFilterState() {
+        var categories = $(".categories-group input");
+        $.each(categories, function (key, value) {
+            if (value.checked) {
+                $(this).next().css({
+                    'color': 'black',
+                    'font-weight': 600
+                })
+                $(this).parent().next().fadeIn();
+            }
+            else {
+                $(this).next().css({
+                    'color': '#808a85',
+                    'font-weight': 400
+                });
+                $(this).parent().next().fadeOut();
+            }
+        });
+    }
+
+});
+
+// ******************************************************************************
+// Scripts for main filter (show tooltip on remove filter icon enter and leave)
+// ******************************************************************************
+
+$(".form-check-toggler").mouseenter(function () {
+    var tooltip = $(".form-check-tooltip", this);
+    tooltip.fadeIn(400);
+});
+
+$(".form-check-toggler").mouseleave(function () {
+    var tooltip = $(".form-check-tooltip", this);
+    setTimeout(() => {
+        tooltip.fadeOut(100);
+    }, 150);
+});
+
+$(".form-check-toggler").click(function () {
+    var categoryInput = $(".category-input", $(this).parent());
+    categoryInput.prop('checked', false);
+    categoryInput.trigger('change');
+});
+
+$(".categories-group input").change(function () {
+    handleHiddenInputLabel($(this));
+});
+
+function handleHiddenInputLabel(input) {
+    var inputChecked = input.prop('checked');
+    var formCheckLabel = input.next();
+    var formCheckToggler = $(".form-check-toggler", input.parent().parent());
+    if (inputChecked) {
+        formCheckLabel.css({
+            'color': 'black',
+            'font-weight': 600
+        });
+        formCheckToggler.fadeIn(200);
+    }
+    else {
+        formCheckLabel.css({
+            'color': '#808a85',
+            'font-weight': 400
+        });
+        formCheckToggler.fadeOut(500);
+    }
+}
+
+// ******************************************************************************
+// ******************************************************************************
